b1cf6d47e99bf8f784650093059a6bb7d1151453
SACHIN SINGH NEGI <sachin.negi@cloudeq.com>
2023-08-24 18:28:37 +0530

    first commit

---
+++ src/Components/Task.jsx
@@ -0,0 +1,403 @@
+import React from "react";
+import "./Task.css";
+import bb from "../Static/b.svg";
+import heart from "../Static/heart.png";
+import ww from "../Static/ww.png";
+
+const Task = () => {
+  return (
+    <>
+      <div className="headerrr">
+        <div className="headerL">
+          <img src={bb} alt="" />
+        </div>
+
+        <div className="headerR">
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <select name="" id="">
+                    <option value="">Templates</option>
+                  </select>
+                </li>
+                <li>
+                  <select name="" id="">
+                    <option value="">Components</option>
+                  </select>
+                </li>
+                <li>Blogs</li>
+                <li>Tags</li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+      </div>
+
+      <div className="jumbotron">
+        <div>
+          <h1>
+            Hello Pro - Html5 Bootstrap One page Business Website Template
+          </h1>
+          <p>Aug 14, 2023</p>
+        </div>
+      </div>
+
+      <div className="middle">
+        <div className="left">
+          <div className="upper">
+            <img src={ww} alt="" />
+          </div>
+          <div className="down">
+            <h4>
+              We Are Giving You Perfect Solutions With Our Proficient Services
+            </h4>
+            <div className="cards">
+              <div>
+                <h6>Machine Vision</h6>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+                  eius minus quaerat officia aspernatur voluptates eum nihil
+                  voluptatum blanditiis? Quia?
+                </p>
+              </div>
+
+              <div>
+                <h6>AI Deployment</h6>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+                  eius minus quaerat officia aspernatur voluptates eum nihil
+                  voluptatum blanditiis? Quia?
+                </p>
+              </div>
+
+              <div>
+                <h6>Robotic Process</h6>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+                  eius minus quaerat officia aspernatur voluptates eum nihil
+                  voluptatum blanditiis? Quia?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="right">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            delectus quo, odit sapiente, molestias error atque, id excepturi
+            ullam debitis iure. Voluptas deserunt id quis voluptate eos amet
+            ipsam minima. iure. Voluptas deserunt id quis voluptate eos amet
+            ipsam minima. iure. Voluptas deserunt id quis voluptate eos amet
+            ipsam minima. iure. Voluptas deserunt id quis voluptate eos amet
+            ipsam minima.
+          </p>
+
+          <h3>Bootstrap One Page Website Template</h3>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit fuga
+            nostrum, eos laboriosam, adipisci perferendis modi ut, labore soluta
+            reiciendis sint architecto id? Est vel ullam, quaerat optio adipisci
+            asperiores! sint architecto id? Est vel ullam, quaerat optio
+            adipisci asperiores! sint architecto id? Est vel ullam, quaerat
+            optio adipisci asperiores! sint architecto id? Est vel ullam,
+            quaerat optio adipisci asperiores!
+          </p>
+          <br />
+
+          <button className="btn1" type="button">
+            <span style={{ marginRight: "5px" }}>&#x1F441; </span>Preview
+          </button>
+          <button className="btn2" type="button">
+            <span>&#128722;</span>Pricing
+          </button>
+          <div>
+            <button className="btn3">
+              <span>Lite Version</span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="middle2">
+        <div>
+          <h4>Version</h4>
+          <p>2.0.0</p>
+        </div>
+        <div>
+          <h4>Last Updated</h4>
+          <p>14 Aug, 2023</p>
+        </div>
+        <div>
+          <h4>Changelog</h4>
+          <p style={{ color: "blue" }}>view</p>
+        </div>
+        <div>
+          <h4>Framework</h4>
+          <p>Bootstrap 5</p>
+        </div>
+      </div>
+
+      <div className="middle33">
+        <div className="middle331">
+          <div className="middle3311">
+            <h2 style={{ paddingLeft: "60px" }}>Highlights</h2>
+          </div>
+
+          <div className="middle3312">
+            <p style={{ fontSize: "20px" }}>Features</p>
+
+            <div className="divs">
+              <ul>
+                <li>Bootstrap5</li>
+                <li>BSB Framework</li>
+                <li>HTML5 and CSS3</li>
+                <li>Responsive Layout</li>
+                <li>Singlr Page Design</li>
+                <li>W3C valid</li>
+                <li>Clean and Commented Code</li>
+                <li>SEO Optinized</li>
+                <li>Cross Browser Compatible</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="middle3313">
+            <p style={{ fontSize: "20px" }}>Pro Features</p>
+
+            <div className="divs">
+              <ul>
+                <li>Pro Components</li>
+                <li>Mouse Interactive Headres</li>
+                <li>PHP/Ajex Contact Form Script</li>
+                <li>SAAS/SCSS Sources Files</li>
+                <li>Premium Support via Email </li>
+                <li>Forum Support</li>
+                <li>Create Websites for Clients</li>
+                <li>White Lable Footer - No Credits</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="middle4">
+        <div className="middle41">
+          <h2>Exclusive</h2>
+        </div>
+        <div className="middle42">
+          <h1>Special Features</h1>
+          <hr />
+          <div className="middle421">
+            <div>
+              <ul>
+                <li>Mouse Interactive Header</li>
+                <li>Blog carousel</li>
+                <li>Fact counter</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Mouse Interactive Header</li>
+                <li>Blog carousel</li>
+                <li>Fact counter</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="middle5">
+        <div className="middle51">
+          <h2>Browsers</h2>
+        </div>
+        <div className="middle52">
+          <div className="middle521">
+            <div style={{ height: "300px" }}>
+              <h2>Category</h2>
+              <div style={{ height: "200px" }}>
+                <p>
+                  Agency, Blog, Business, Consulting, Corporate, Creative,
+                  Finance, Landing Page, Marketing, Pro, Small Business, Startup
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2>Tags</h2>
+              <div>
+                <p>
+                  Agency, Blog, Business, Consulting, Corporate, Creative,
+                  Finance, Landing Page, Marketing, Pro, Small Business, Startup
+                  Blog, Business, Consulting, Blog, Business, Consulting,
+                  Corporate, Creative, Finance, Consulting, Corporate,
+                  Creative,Consulting, Corporate, Creative,
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+
+        <div>
+          <table id="alter">
+            <tr >
+              <td id="tdfirst"></td>
+              <td id="tds">
+                <div id="div1">
+                  <h1>Free </h1>
+                  <p>personal</p>
+                </div>
+                <h1 style={{fontSize:"70px" , fontWeight:"bold"}} >$<span style={{verticalAlign:"sub"}}>0</span></h1>
+                <div id="div2">
+                  <p>Lifetime access</p>
+                </div>
+              </td>
+              <td id="tds">
+                <div id="div1">
+                <h1>Pro </h1>
+                  <p>Developer</p>
+                </div>
+                <h1 style={{fontSize:"70px" , fontWeight:"bold"}} >$<span style={{verticalAlign:"sub"}}>29</span></h1>
+                
+                <div id="div2">
+                <p style={{width:"100px", marginLeft:"70px"}}><span>12</span> Months Access Single Domain</p>
+                </div>
+              </td>
+              <td id="tds">
+                <div id="div1">
+                <h1>BB Startup </h1>
+                  <p>Freelancer</p>
+                </div>
+                <h1 style={{fontSize:"70px" , fontWeight:"bold"}} >$<span style={{verticalAlign:"sub"}}>49</span></h1>
+
+                <div id="div2">
+                <p><span>3</span>Mpnths Access</p>
+                <p><span>4</span> Pro BS Templates</p>
+                <p><span>27</span>Pro BS Components</p>
+                </div>
+              </td>
+              <td id="tds">
+                <div id="div1">
+                <h1>BB Club </h1>
+                  <p>agency</p>
+                </div>
+                <h1 style={{fontSize:"70px" , fontWeight:"bold"}} >$<span style={{verticalAlign:"sub"}}>149</span></h1>
+                <div id="div2">
+                <p><span>12</span>Mpnths Access</p>
+                <p><span>4</span> Pro BS Templates</p>
+                <p><span>27</span>Pro BS Components</p>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td>Harry Depp</td>
+              <td>Harry Depp</td>
+              <td>Britain</td>
+              <td>Britain</td>
+              <td>Britain</td>
+            </tr>
+            <tr>
+              <td style={{ backgroundColor: "rgb(232, 236, 238)" }}></td>
+              <td style={{ height: "90px",backgroundColor:"#85c0ff" }}>
+
+                <button className="buttond" style={{backgroundColor:"rgb(16, 189, 16)"}}>Free Download</button>
+
+
+              </td>
+              <td style={{ height: "90px",backgroundColor:"#85c0ff" }}>
+                <button className="buttond">Buy Now</button>
+              </td>
+              <td style={{ height: "90px",backgroundColor:"#85c0ff" }}>
+                <button className="buttond">Buy Now</button>
+              </td>
+              <td style={{ height: "90px",backgroundColor:"#85c0ff" }}>
+                <button className="buttond">Buy Now</button>
+              </td>
+            </tr>
+
+          </table>
+        </div>
+
+
+
+
+      </div>
+    </>
+  );
+};
+
+export default Task;
